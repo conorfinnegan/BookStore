6bb36f63df10304894407c0854b1cc56a450423b
conorfinnegan <confinn93@gmail.com>
2016-04-12 01:37:56 +0100

    Removed Customers ability to edit stock amount

--- app/src/main/java/com/example/conorfinnegan/bookstore/CustomerViewBook.java
+++ app/src/main/java/com/example/conorfinnegan/bookstore/CustomerViewBook.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -71,20 +69,6 @@ public class CustomerViewBook extends AppCompatActivity implements View.OnClickL
 
         getData();
     }
-
-    private final TextWatcher quantityWatcher = new TextWatcher() {
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-        }
-
-        public void afterTextChanged(Editable s) {
-            editTextInvisible.setText(editTextQuantity.getText());
-            editTextInvisible.setVisibility(View.GONE);
-        }
-    };
 
 
     private void getData() {
