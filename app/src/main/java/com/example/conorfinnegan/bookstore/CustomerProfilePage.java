d496976a90bdc31480674a6ad5969ce898afdd6d
conorfinnegan <confinn93@gmail.com>
2016-04-12 01:38:10 +0100

    Tidied up existing code

--- app/src/main/java/com/example/conorfinnegan/bookstore/CustomerProfilePage.java
+++ app/src/main/java/com/example/conorfinnegan/bookstore/CustomerProfilePage.java
@@ -10,13 +10,8 @@ import android.widget.TextView;
 public class CustomerProfilePage extends AppCompatActivity implements View.OnClickListener {
     private TextView textView;
     private Button btnLinkToSearch;
-    private Button btnLinkToDelete;
     private Button btnLinkToViewCart;
-    private Button btnLinkToViewShortList;
     private Button btnLinkToLogout;
-    private Button btnLinkToSuggestedOpponents;
-    private Button btnLinkToViewRequests;
-    private Button btnLinkToViewFixtures;
 
     public static final String USER_NAME = "USER_NAME";
 
@@ -33,75 +28,39 @@ public class CustomerProfilePage extends AppCompatActivity implements View.OnCli
 
         username = intent.getStringExtra("username");
 
-        textView.setText("Loged In as: "+username);
+        textView.setText("Logged In as: "+username);
 
         btnLinkToSearch = (Button) findViewById(R.id.SearchBook);
 
         btnLinkToViewCart= (Button) findViewById(R.id.ViewCart);
-//
-//        btnLinkToViewAll = (Button) findViewById(R.id.ViewAll);
-//
-//        btnLinkToViewShortList= (Button) findViewById(R.id.ViewShortList);
-//
-//        btnLinkToSuggestedOpponents = (Button) findViewById(R.id.SuggestedOpponents);
-//
-//        btnLinkToViewRequests = (Button) findViewById(R.id.IncomingRequests);
-//
+
         btnLinkToLogout= (Button) findViewById(R.id.Logout);
-//
-//        btnLinkToViewFixtures = (Button) findViewById(R.id.ViewFixtures);
-//
+
         btnLinkToSearch.setOnClickListener(this);
 
         btnLinkToViewCart.setOnClickListener(this);
 
-//        btnLinkToViewAll.setOnClickListener(this);
-//
-//        btnLinkToViewShortList.setOnClickListener(this);
-//
-//        btnLinkToSuggestedOpponents.setOnClickListener(this);
-//
-//        btnLinkToViewRequests.setOnClickListener(this);
-//
         btnLinkToLogout.setOnClickListener(this);
-//
-//        btnLinkToViewFixtures.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if(v == btnLinkToSearch){
+            Intent newintent = getIntent();
+            finish();
+            startActivity(newintent);
             Intent intent = new Intent(CustomerProfilePage.this,CustomerSearch.class);
             intent.putExtra("username", username);
             startActivity(intent);
         }
         if(v == btnLinkToViewCart){
+            Intent newintent = getIntent();
+            finish();
+            startActivity(newintent);
             Intent intent = new Intent(CustomerProfilePage.this,ViewCart.class);
             intent.putExtra("username", username);
             startActivity(intent);
         }
-//        if(v == btnLinkToViewAll){
-//            Intent intent = new Intent(ProfileActivity.this,ViewAllActivity.class);
-//            intent.putExtra(USER_NAME, username);
-//            startActivity(intent);
-//        }
-//        if(v == btnLinkToViewShortList){
-//            Intent intent = new Intent(ProfileActivity.this,ViewShortlist.class);
-//            intent.putExtra("username", username);
-//            startActivity(intent);
-//        }
-//
-//        if(v == btnLinkToSuggestedOpponents){
-//            Intent intent = new Intent(ProfileActivity.this,SuggestedOpponents.class);
-//            intent.putExtra("username", username);
-//            startActivity(intent);
-//        }
-//
-//        if(v == btnLinkToViewRequests){
-//            Intent intent = new Intent(ProfileActivity.this,ViewRequests.class);
-//            intent.putExtra("username", username);
-//            startActivity(intent);
-//        }
 
         if(v == btnLinkToLogout){
             this.finish();
@@ -110,11 +69,5 @@ public class CustomerProfilePage extends AppCompatActivity implements View.OnCli
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
-
-//        if(v == btnLinkToViewFixtures){
-//            Intent intent = new Intent(ProfileActivity.this,ViewFixtures.class);
-//            intent.putExtra("username", username);
-//            startActivity(intent);
-//        }
     }
 }
